8f5e2a1ab2afc1ed24c6ef6b3c031d6393a608d2
Rajeev Ram <rajeevram@email.arizona.edu>
2018-07-26 16:23:50 -0700

    Finished third user story

--- Flix/Flix/NowPlayingViewController.swift
+++ Flix/Flix/NowPlayingViewController.swift
@@ -14,16 +14,29 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     // UI, UX Variables
     @IBOutlet weak var movieTableView: UITableView!
     
+    // Backend Variables
     var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         // Superclass initializer
         super.viewDidLoad()
         
+        // Setup pull-to-refresh functionality
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.pullToRefresh(_:)), for: .valueChanged)
+        movieTableView.insertSubview(refreshControl, at: 0)
+        
+        // Properly source and align table view
         movieTableView.dataSource = self as UITableViewDataSource
         movieTableView.rowHeight = 170
         
-        // Making a network request
+        // Make a network request
+        getNowPlayingMovies()
+    }
+    
+    // Make a network request to fetch movies
+    func getNowPlayingMovies() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
@@ -36,11 +49,17 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 let dictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 self.movies = dictionary["results"] as! [[String: Any]]
                 self.movieTableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
         task.resume()
     }
-
+    
+    // Resend the network request upon pull
+    @objc func pullToRefresh(_ refreshControl: UIRefreshControl) {
+        getNowPlayingMovies()
+    }
+    
     // Superclass methods
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -52,19 +71,20 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Retrieve JSON information
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        
         let movie = movies[indexPath.row]
+        
+        // Title and description of movie
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        
         cell.titleLabel.text = title
         cell.descriptionLabel.text = overview
         
+        // Poster image with AlamoFire
         let poster = movie["poster_path"] as! String
         let baseURL = "https://image.tmdb.org/t/p/w500"
         let posterURL = URL(string: baseURL + poster)!
-        
         cell.posterImageView.af_setImage(withURL: posterURL)
         
         return cell
